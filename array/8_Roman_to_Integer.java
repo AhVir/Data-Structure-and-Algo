944d4ac593b772b2a0459647ef7fce68f53a41f8
Tanvir Ahmed <110638482+AhVir@users.noreply.github.com>
2025-11-16 00:23:30 +0600

    Add alternative approaches as comments in romanToInt
    Commented out alternative approaches for converting Roman numerals to integers.

--- array/8_Roman_to_Integer.java
+++ array/8_Roman_to_Integer.java
@@ -1,5 +1,46 @@
 class Solution {
     public int romanToInt(String s) {
+        // Some Other Approach
+        // -------------------
+        
+        // int res = 0;
+
+        // Map<String, Integer> val = new HashMap<>();
+        // val.put("I", 1);
+        // val.put("V", 5);
+        // val.put("X", 10);
+        // val.put("L", 50);
+        // val.put("C", 100);
+        // val.put("D", 500);
+        // val.put("M", 1000);
+        // val.put("IV", 4);
+        // val.put("IX", 9);
+        // val.put("XL", 40);
+        // val.put("XC", 90);
+        // val.put("CD", 400);
+        // val.put("CM", 900);
+        
+        // for(int i=0; i<s.length(); i++){
+        //     if(i+1 < s.length()){
+        //         String str = s.substring(i, i+2);
+        //         if(val.containsKey(str)){
+        //             res += val.get(str);
+        //             i++;
+        //             continue;
+        //         }
+        //     }
+
+        //     String str = s.substring(i, i+1);
+        //     res += val.get(str);
+        // }
+
+        // return res;
+
+
+        
+        // Another ONE
+        // ----------
+        
         int res = 0;
 
         Map<Character, Integer> val = new HashMap<>();
