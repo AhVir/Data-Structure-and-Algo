22b158334d133f3dfbeacd1a464dc30dd8a086c4
Tanvir Ahmed <110638482+AhVir@users.noreply.github.com>
2025-11-15 07:35:00 +0600

    Update 4_valid-anagram.java

--- array/4_valid-anagram.java
+++ array/4_valid-anagram.java
@@ -1,24 +1,44 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] freqS = new int[26];
-        int[] freqT = new int[26];
+        // int[] freqS = new int[26];
+        // int[] freqT = new int[26];
 
         if(s.length() != t.length()) return false;
 
-        for(char c:s.toCharArray()){
-            int temp = c-'a';
-            freqS[temp]++;
+        // 1st Approach:
+        // ------------
+
+        // for(char c:s.toCharArray()){
+        //     int temp = c-'a';
+        //     freqS[temp]++;
+        // }
+
+        // for(char c:t.toCharArray()){
+        //     int temp = c-'a';
+        //     freqT[temp]++;
+        // }
+
+
+        // for(int i=0; i<26; i++){
+        //     if(freqS[i] != freqT[i]) return false;
+        // }
+
+
+
+        // 2nd Approach:
+        // -------------
+
+        int[] freq = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            int char1 = s.charAt(i) - 'a';
+            int char2 = t.charAt(i) - 'a';
+
+            freq[char1]++;
+            freq[char2]--;
         }
 
-        for(char c:t.toCharArray()){
-            int temp = c-'a';
-            freqT[temp]++;
-        }
-
-
-        for(int i=0; i<26; i++){
-            if(freqS[i] != freqT[i]) return false;
-        }
+        for(int x:freq) if(x != 0) return false;
 
         return true;
     }
