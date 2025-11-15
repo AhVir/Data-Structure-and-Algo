854c9b579efc76592de04a91d5bdcd5222fd625e
Tanvir Ahmed <110638482+AhVir@users.noreply.github.com>
2025-11-15 07:16:47 +0600

    Implement isAnagram method to check anagrams

---
+++ array/4_valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] freqS = new int[26];
+        int[] freqT = new int[26];
+
+        for(char c:s.toCharArray()){
+            int temp = c-'a';
+            freqS[temp]++;
+        }
+
+        for(char c:t.toCharArray()){
+            int temp = c-'a';
+            freqT[temp]++;
+        }
+
+
+        for(int i=0; i<26; i++){
+            if(freqS[i] != freqT[i]) return false;
+        }
+
+        return true;
+    }
+}
