9afa9e5929c33a3ed53daea64a29e3b726b3c3a9
Tanvir Ahmed <110638482+AhVir@users.noreply.github.com>
2025-11-15 23:49:35 +0600

    Implement Roman to Integer conversion

--- array/8_Roman_to_Integer.java
+++ array/8_Roman_to_Integer.java
@@ -1,1 +1,32 @@
+class Solution {
+    public int romanToInt(String s) {
+        int res = 0;
 
+        Map<Character, Integer> val = new HashMap<>();
+        val.put('I', 1);
+        val.put('V', 5);
+        val.put('X', 10);
+        val.put('L', 50);
+        val.put('C', 100);
+        val.put('D', 500);
+        val.put('M', 1000);
+        
+        for(int i=0; i<s.length(); i++){
+            if (i + 1 < s.length()) {
+                char curr = s.charAt(i);
+                char next = s.charAt(i + 1);
+
+                if (curr == 'I' && next == 'V') { res += 4; i += 1; continue; }
+                if (curr == 'I' && next == 'X') { res += 9; i += 1; continue; }
+                if (curr == 'X' && next == 'L') { res += 40; i += 1; continue; }
+                if (curr == 'X' && next == 'C') { res += 90; i += 1; continue; }
+                if (curr == 'C' && next == 'D') { res += 400; i += 1; continue; }
+                if (curr == 'C' && next == 'M') { res += 900; i += 1; continue; }
+            }
+            
+            res += val.get(s.charAt(i));
+        }
+
+        return res;
+    }
+}
