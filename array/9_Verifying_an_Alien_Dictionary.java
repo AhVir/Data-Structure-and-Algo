057d12a37fc33515a5b1b1c50bd5a70b4e95d6be
Tanvir Ahmed <110638482+AhVir@users.noreply.github.com>
2025-11-16 11:50:19 +0600

    Implement alien dictionary verification method

--- array/9_Verifying_an_Alien_Dictionary.java
+++ array/9_Verifying_an_Alien_Dictionary.java
@@ -1,1 +1,36 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        Map<Character, Integer> orderMap = new HashMap<>();
+        
+        int i = 0;
+        for(char x:order.toCharArray()){
+            orderMap.put(x, i++);
+        }
 
+        for(i=0; i<words.length-1; i++){
+            int word1 = words[i].length();
+            int word2 = words[i+1].length();
+
+            // if(word1 > word2) return false;
+
+            int len = Math.min(word1, word2);
+            boolean wasBroken = false;
+
+            for(int j=0; j<len; j++){
+                int val1 = orderMap.get(words[i].charAt(j));
+                int val2 = orderMap.get(words[i+1].charAt(j));
+
+                if(val1 > val2) return false;
+                if(val1 < val2) {
+                    wasBroken = true;
+                    break;
+                }
+            }
+
+            // if(word1 > word2) return false;
+            if(!wasBroken && (word1 > word2)) return false;
+        }
+
+        return true;
+    }
+}
