a23fa272654350aa5ea692770c73547bb663c0d8
Tanvir Ahmed <110638482+AhVir@users.noreply.github.com>
2025-11-17 00:26:35 +0600

    Implement longest consecutive sequence algorithm

--- array/10_Longest_Consecutive_Sequence.java
+++ array/10_Longest_Consecutive_Sequence.java
@@ -1,1 +1,27 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0) return 0;
 
+        HashSet<Integer> numbers = new HashSet<>();
+        for(int x:nums){
+            numbers.add(x);
+        }
+
+        int lcs = 1;
+
+        for(int num:numbers){
+            if(numbers.contains(num-1)) continue;
+
+            int currentLCS = 1;
+            int currentNum = num;
+            while(numbers.contains(currentNum+1)){
+                currentLCS++;
+                currentNum++;
+            }
+
+            lcs = Math.max(lcs, currentLCS);
+        }
+
+        return lcs;
+    }
+}
